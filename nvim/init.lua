8cd0d7247b2efebca7dae1aa9c3cfd17cb21bb3b
Luca Trușcă <t.luca1152@gmail.com>
2024-01-21 15:36:34 +0200

    chore(nvim): use lazy.nvim instead of Plug

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,12 +26,25 @@ vim.api.nvim_set_keymap('n', '<leader>Y', 'gg"+yG', { noremap = true, silent = t
 vim.api.nvim_set_keymap('x', 'J', ':m \'>+1<CR>gv=gv', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('x', 'K', ':m \'<-2<CR>gv=gv', { noremap = true, silent = true })
 
--- Plugins
-vim.cmd([[call plug#begin(stdpath('data') .. '/plugged')]])
-vim.cmd([[Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }]])
-vim.cmd([[Plug 'junegunn/fzf.vim']])
-vim.cmd([[Plug 'catppuccin/nvim', { 'as': 'catppuccin' }]])
-vim.cmd([[call plug#end()]])
+-- lazy.nvim setup
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- lazy.nvim plugins
+require("lazy").setup({
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    "williamboman/mason.nvim",
+})
 
 -- Color scheme
 vim.cmd([[colorscheme catppuccin-mocha]])
