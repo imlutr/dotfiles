95f07ff9817df4ba66b4dc9cfdd0de6c047208c4
Luca Trușcă <ltrusca@adobe.com>
2024-01-22 16:53:52 +0200

    chore(nvim): make tabs take 4 spaces

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,2 +1,8 @@
 -- Don't yank to system clipboard
 vim.opt.clipboard = ""
+
+-- Tab settings
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
