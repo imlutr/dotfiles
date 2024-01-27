6bbd2e8b5d5d6e727647e3e60e0800f725e21be8
Luca Trușcă <t.luca1152@gmail.com>
2024-01-27 14:19:50 +0200

    chore(nvim): change shortcuts for harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -9,14 +9,14 @@ return {
     end,
     keys = {
       {
-        "<leader>A",
+        "<leader>H",
         function()
           require("harpoon"):list():append()
         end,
         desc = "Harpoon current file",
       },
       {
-        "<leader>a",
+        "<leader>h",
         function()
           local harpoon = require("harpoon")
           harpoon.ui:toggle_quick_menu(harpoon:list())
