1e647226a6e8f73ca139ea4ee98c8c11a2099884
lutrdev <t.luca1152@gmail.com>
2024-04-26 22:05:23 +0300

    chore(nvim): nuke configuration

--- nvim/lua/config/keymaps.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_set_keymap("n", "<leader>pv", ":Vex<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-p>", ":GFiles<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>pf", ":Files<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "<leader>p", '"_dP', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>y", '"+y', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>Y", 'gg"+yG', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
--- nvim/lua/plugins/disabled.lua
+++
@@ -1,4 +0,0 @@
-return {
-  { "folke/flash.nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-}
